58f597be1e97a61b4bb15c924ee4ce8500f80d43
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-14 23:45:39 +0300

    hotfixes error 500 and valid token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,8 +1,8 @@
 //* Импортируем модуль jsonwebtoken для верификации токена
 const jwt = require('jsonwebtoken');
 
-// //* Подключаем модуль для проверки данных на тип
-// const validator = require('validator');
+//* Подключаем модуль для проверки данных на тип
+const validator = require('validator');
 
 //* Импорт констант
 const { textErrorAuthRequired } = require('../utils/constants');
@@ -10,48 +10,36 @@ const { textErrorAuthRequired } = require('../utils/constants');
 //* Импорт классового элемента ошибки
 const AuthError = require('../errors/AuthError');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { NODE_ENV, JWT_SECRET } = process.env;
 
-  let token;
-
-  const checkedTokenCookies = () => {
-    const tokenJwt = req.cookies.jwt;
-    if (!tokenJwt) {
-      throw (new AuthError(textErrorAuthRequired));
+  const checkedToken = (token) => {
+    if (!token || !validator.isJwt(token)) {
+      throw new AuthError(textErrorAuthRequired);
     }
-    token = tokenJwt;
-  };
-
-  const checkedTokenHeaders = () => {
-    const { authorization } = req.headers;
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      throw (new AuthError(textErrorAuthRequired));
+    try {
+      return jwt.verify(
+        token,
+        NODE_ENV === 'production'
+          ? JWT_SECRET
+          : 'keyword-for-token-generation',
+      );
+    } catch (err) {
+      throw new AuthError(textErrorAuthRequired);
     }
-    token = authorization.replace(/^\S+/, '').trim();
+
+    // token = authorization.replace(/^\S+/, '').trim();
   };
 
   if (NODE_ENV) {
-    checkedTokenCookies();
+    const tokenJwt = req.cookies.jwt;
+    req.user = checkedToken(tokenJwt);
   } else {
-    checkedTokenHeaders();
+    const { authorization } = req.headers;
+    // authorization.startsWith('Bearer ')
+    const tokenJwt = authorization ? authorization.substring(7) : null;
+    req.user = checkedToken(tokenJwt);
   }
-
-  let payload;
-
-  try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production'
-        ? JWT_SECRET
-        : 'keyword-for-token-generation',
-    );
-  } catch (err) {
-    next(new AuthError(textErrorAuthRequired));
-  }
-
-  req.user = payload;
 
   next();
 };
