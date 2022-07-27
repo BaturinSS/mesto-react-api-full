f62dbc3940ec9291b995774cba09fcfdb9bb8db0
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-27 17:15:13 +0300

    test

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,14 +8,23 @@ const { keywordTokenDev } = require('../utils/constants');
 const AuthError = require('../errors/AuthError');
 
 module.exports = (req, res, next) => {
+<<<<<<< Updated upstream
   const { NODE_ENV, JWT_SECRET = keywordTokenDev } = process.env;
+=======
+  const { NODE_ENV, JWT_SECRET } = process.env;
+>>>>>>> Stashed changes
 
   const checkedToken = (token) => {
     if (!token) {
       throw new AuthError();
     }
     try {
-      return jwt.verify(token, JWT_SECRET);
+      return jwt.verify(
+        token,
+        NODE_ENV === 'production'
+          ? JWT_SECRET
+          : 'keyword-for-token-generation',
+      );
     } catch (err) {
       throw new AuthError();
     }
@@ -26,9 +35,7 @@ module.exports = (req, res, next) => {
     req.user = checkedToken(tokenJwt);
   } else {
     const { authorization } = req.headers;
-    const tokenJwt = (authorization && authorization.startsWith('Bearer '))
-      ? authorization.substring(7)
-      : null;
+    const tokenJwt = (authorization && authorization.startsWith('Bearer ')) ? authorization.substring(7) : null;
     req.user = checkedToken(tokenJwt);
   }
 
