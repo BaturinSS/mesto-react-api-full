d906fa6370f50f02d8f334232a733b05b11f5871
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-08-01 22:26:55 +0300

    ++

--- frontend/src/components/EditProfilePopup/EditProfilePopup.js
+++ frontend/src/components/EditProfilePopup/EditProfilePopup.js
@@ -12,16 +12,21 @@ function EditProfilePopup({
   isButtonDisabled,
   setIsButtonDisabled
 }) {
-  const { setIsEventInput,
+  const {
+    setIsEventInput,
     setIsOpenForm, isValidForm,
-    isValidInput, isErrorMessage
+    isValidInput, isErrorMessage,
   } = FormValidator();
 
-  const { nameErrorMessage = '',
-    jobErrorMessage = '' } = isErrorMessage;
+  const {
+    nameErrorMessage = '',
+    jobErrorMessage = '',
+  } = isErrorMessage;
 
-  const { nameValidInput = true,
-    jobValidInput = true } = isValidInput;
+  const {
+    nameValidInput = true,
+    jobValidInput = true,
+  } = isValidInput;
 
   const [name, setName] = useState({});
 
@@ -50,48 +55,48 @@ function EditProfilePopup({
     setIsButtonDisabled(true);
     onUpdateUser(name, description);
   }
-
   return (
-    <PopupWithForm
-      name="profile-edit"
-      title="Редактировать профиль"
-      buttonText={downloadText ? 'Сохраняем...' : 'Сохранить'}
-      isOpen={isOpen}
-      onClose={onClose}
-      onSubmit={handleSubmit}
-      isButtonDisabled={isButtonDisabled}
-      isValidForm={isValidForm}
-    >
-      <input
-        id="userName-input"
-        className={`popup__input popup__input_user-name ${!nameValidInput ? "popup__input_type_error" : ''}`}
-        required
-        placeholder="Имя"
-        spellCheck="true"
-        type="text"
-        name="name"
-        minLength="4"
-        maxLength="40"
-        onChange={handleNameChange}
-        value={name || ''}
-      />
-      <span className={`userName-input-error popup__input-error ${!nameValidInput ? "popup__input-error_active" : ''}`}>{nameErrorMessage}</span>
-      <input
-        id="userProfession-input"
-        className={`popup__input popup__input_user-profession ${!jobValidInput ? "popup__input_type_error" : ''}`}
-        required
-        placeholder="О себе"
-        spellCheck="true"
-        type="text"
-        name="job"
-        minLength="4"
-        maxLength="200"
-        onChange={handleJobChange}
-        value={description || ''}
-      />
-      <span className={`userProfession-input-error popup__input-error ${!jobValidInput ? "popup__input-error_active" : ''}`}>{jobErrorMessage}</span>
-    </PopupWithForm>
-  )
+    <>
+      < PopupWithForm
+        name="profile-edit"
+        title="Редактировать профиль"
+        buttonText={downloadText ? 'Сохраняем...' : 'Сохранить'}
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={handleSubmit}
+        isButtonDisabled={isButtonDisabled}
+        isValidForm={isValidForm}
+      >
+        <input
+          id="userName-input"
+          className={`popup__input popup__input_user-name ${!nameValidInput ? "popup__input_type_error" : ''}`}
+          required
+          placeholder="Имя"
+          spellCheck="true"
+          type="text"
+          name="name"
+          minLength="4"
+          maxLength="40"
+          onChange={handleNameChange}
+          value={name || ''}
+        />
+        <span className={`userName-input-error popup__input-error ${!nameValidInput ? "popup__input-error_active" : ''}`}>{nameErrorMessage}</span>
+        <input
+          id="userProfession-input"
+          className={`popup__input popup__input_user-profession ${!jobValidInput ? "popup__input_type_error" : ''}`}
+          required
+          placeholder="О себе"
+          spellCheck="true"
+          type="text"
+          name="job"
+          minLength="4"
+          maxLength="200"
+          onChange={handleJobChange}
+          value={description || ''}
+        />
+        <span className={`userProfession-input-error popup__input-error ${!jobValidInput ? "popup__input-error_active" : ''}`}>{jobErrorMessage}</span>
+      </>
+      )
 }
 
-export default EditProfilePopup;
+      export default EditProfilePopup;
